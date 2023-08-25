4bfbad40cc14ad686068fae3bada348bd390ce8b
JJMarin-08345 <marinjuanjose2005@gmail.com>
2023-08-24 20:03:27 -0500

    Ahorcado funcionando

--- ahorcado.py
+++ ahorcado.py
@@ -1,25 +1,56 @@
 #Juego de ahorcado
-print("*** BIENVENIDO AL JUEGO DE AHORCADO ***")
+print("\n\n*** BIENVENIDO AL JUEGO DE AHORCADO ***")
 Palabra=input("Digite una palabra: ").upper()
 Palabra=list(Palabra)
-Vidas=5
-Acerto=False
+
 
 def isLetra():
     while True:
         try:
-            Letra = input("\n\n Ingrese una letra: ")
+            Letra = input("\n\n Ingrese una letra: ").upper()
             if len(Letra) == 1 and Letra.isalpha():
-                print("Ingreso válido.")
                 break
             else:
                 print("\nSe detectó más de una letra o caracteres no alfabéticos.")
         except ValueError:
             None
     return Letra
-    
-Letra=''
 
-for i in range(5):
-    for j in range(len(Palabra)):
-        Letra=isLetra()
+
+def Ahorcado(Palabra, Intentos, Pb_Oculta, cntAcertadas):
+    lgtd_Pb = len(Palabra)
+
+    if lgtd_Pb == cntAcertadas:
+        print("FELICIDADES, HAS DESCUBIERTO LA PALABRA")
+        Intentos = -200
+        return Intentos, cntAcertadas  # Salir de la función si se ha ganado
+
+    AdivinoLetra = False
+    Letra = isLetra()
+
+    for i in range(len(Palabra)):
+        if Letra in Palabra[i]:
+            Pb_Oculta[i] = Letra
+            cntAcertadas += 1
+            AdivinoLetra = True
+
+    if AdivinoLetra:
+        print("Correcto, has acertado una(s) letra(s)")
+        print("\n", (" ").join(Pb_Oculta))
+    else:
+        Intentos -= 1
+        print(f"Incorrecto, le quedan {Intentos} vidas")
+
+    return Intentos, cntAcertadas
+
+
+Palabra_Oculta = ['_'] * len(Palabra)
+Vidas = 5
+Acertadas = 0
+while Vidas > 0:
+    Vidas, Acertadas = Ahorcado(Palabra, Vidas, Palabra_Oculta, Acertadas)
+    if (Vidas == -200):  # Salir del ciclo si se ha ganado
+        break
+    
+if(Vidas==0):
+    print(f"\n\nHAS PERDIDO, LA PALABRA ERA: {Palabra}\n HAS ACERTADO {Acertadas} LETRA(S)")
