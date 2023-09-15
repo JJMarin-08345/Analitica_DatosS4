5cc6dd1ceb92eb79fda97045d43c6f2541c33757
JJMarin-08345 <marinjuanjose2005@gmail.com>
2023-09-14 19:14:33 -0500

    Analisis Basico - V0.1.0

--- AnalisisBD/SolucionBD.py
+++ AnalisisBD/SolucionBD.py
@@ -23,5 +23,20 @@ PRP_101_200 = Data.loc[ (Data[:][8]>=101) & (Data[:][8]<=200) ]
 PRP_201_300 = Data.loc[ (Data[:][8]>=201) & (Data[:][8]<=300) ]
 PRP_301_400 = Data.loc[ (Data[:][8]>=301) & (Data[:][8]<=400) ]
 PRP_401_500 = Data.loc[ (Data[:][8]>=401) & (Data[:][8]<=500) ]
-PRP_401_600 = Data.loc[ (Data[:][8]>=501) & (Data[:][8]<=600) ]
+PRP_501_600 = Data.loc[ (Data[:][8]>=501) & (Data[:][8]<=600) ]
 PRP_600 = Data.loc[ Data[:][8]>=600 ]
+
+NumSamplesPRP1 = (PRP_0_20.shape[0])
+NumSamplesPRP2 = (PRP_21_100.shape[0])
+NumSamplesPRP3 = (PRP_101_200.shape[0])
+NumSamplesPRP4 = (PRP_201_300.shape[0])
+NumSamplesPRP5 = (PRP_301_400.shape[0])
+NumSamplesPRP6 = (PRP_401_500.shape[0])
+NumSamplesPRP7 = (PRP_501_600.shape[0])
+NumSamplesPRP8 = (PRP_600.shape[0])
+
+## Imprimiendo resultados
+print('Número de muestras totales: ', NumSamples)
+print('Número de muestras por clase 1: ',NumSamplesPRP1,' (',100*NumSamplesPRP1/NumSamples,' %)')
+print('Número de muestras por clase 2: '+str(NumSamplesPRP2)+' ('+str(round(100*NumSamplesPRP2/NumSamples))+' %)')
+print('Número de muestras por clase 3: '+str(NumSamplesPRP3)+' ('+str(round(100*NumSamplesPRP3/NumSamples))+' %)')
