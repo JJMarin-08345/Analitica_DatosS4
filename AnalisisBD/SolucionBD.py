8e8282bb31c750e0e41f0d029c79c0c67180daed
JJMarin-08345 <marinjuanjose2005@gmail.com>
2023-09-14 19:29:52 -0500

    BD Analizada hasta numpy

--- AnalisisBD/SolucionBD.py
+++ AnalisisBD/SolucionBD.py
@@ -14,9 +14,11 @@ Data = pd.read_csv(DataSetName, sep=',', header=None)
 # print(Data.head())
 # print("\n",Data.describe())
 
+#Numero de clases completas
 NumSamples = Data.shape[0]#Escogemos todas las filas
-#print(NumSamples)
 
+
+#Filtro muestras por clases
 PRP_0_20 = Data.loc[ (Data[:][8]>=0) & (Data[:][8]<=20) ]
 PRP_21_100 = Data.loc[ (Data[:][8]>=21) & (Data[:][8]<=100) ]
 PRP_101_200 = Data.loc[ (Data[:][8]>=101) & (Data[:][8]<=200) ]
@@ -26,6 +28,7 @@ PRP_401_500 = Data.loc[ (Data[:][8]>=401) & (Data[:][8]<=500) ]
 PRP_501_600 = Data.loc[ (Data[:][8]>=501) & (Data[:][8]<=600) ]
 PRP_600 = Data.loc[ Data[:][8]>=600 ]
 
+#Numero de muestras por clases
 NumSamplesPRP1 = (PRP_0_20.shape[0])
 NumSamplesPRP2 = (PRP_21_100.shape[0])
 NumSamplesPRP3 = (PRP_101_200.shape[0])
@@ -40,3 +43,21 @@ print('Número de muestras totales: ', NumSamples)
 print('Número de muestras por clase 1: ',NumSamplesPRP1,' (',100*NumSamplesPRP1/NumSamples,' %)')
 print('Número de muestras por clase 2: '+str(NumSamplesPRP2)+' ('+str(round(100*NumSamplesPRP2/NumSamples))+' %)')
 print('Número de muestras por clase 3: '+str(NumSamplesPRP3)+' ('+str(round(100*NumSamplesPRP3/NumSamples))+' %)')
+print('Número de muestras por clase 4: '+str(NumSamplesPRP4)+' ('+str(round(100*NumSamplesPRP4/NumSamples))+' %)')
+print('Número de muestras por clase 5: '+str(NumSamplesPRP5)+' ('+str(round(100*NumSamplesPRP5/NumSamples))+' %)')
+print('Número de muestras por clase 6: '+str(NumSamplesPRP6)+' ('+str(round(100*NumSamplesPRP6/NumSamples))+' %)')
+print('Número de muestras por clase 7: '+str(NumSamplesPRP7)+' ('+str(round(100*NumSamplesPRP7/NumSamples))+' %)')
+print('Número de muestras por clase 8: '+str(NumSamplesPRP8)+' ('+str(round(100*NumSamplesPRP8/NumSamples))+' %)')
+
+## Cambio de datos a tipo numpy
+DataAsNumpy = np.concatenate((PRP_0_20.values, PRP_21_100.values, PRP_101_200.values, PRP_201_300.values,
+                               PRP_301_400.values, PRP_401_500.values, PRP_501_600.values, PRP_600.values), axis=0)
+
+X = DataAsNumpy[:,1:9]
+Y = DataAsNumpy[:,9]
+
+print(X.shape)
+print(Y.shape)
+
+print(X)
+print(Y)
